d6071219e431be6335b4821fd81328ed5ac7332f
olaf <juliendupdupdup@gmail.com>
2024-01-07 19:13:01 +0100

    add progile component

---
+++ src/components/Profile.jsx
@@ -0,0 +1,20 @@
+function Profile() {
+
+    return (
+      <>
+        {/* <Header/> */}
+        {/* <div className="main">
+          <SideBar/> */}
+          <main>
+            <h1>Bonjour Thomas
+            </h1>
+          </main>
+        {/* </div> */}
+  
+      
+       
+      </>
+    )
+  }
+  
+  export default Profile
