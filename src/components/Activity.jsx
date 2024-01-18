ce92813dbacff38940d4cc0063e3f3b7abc414da
olaf <juliendupdupdup@gmail.com>
2024-01-18 16:29:24 +0100

    slash added in src path

--- src/components/Activity.jsx
+++ src/components/Activity.jsx
@@ -2,7 +2,7 @@ export default function Activity(prop) {
 
     return (
       <div className="activity">
-        <img src={`src/assets/${prop.activity}.svg`} alt={prop.activity} />
+        <img src={`/src/assets/${prop.activity}.svg`} alt={prop.activity} />
       </div>
     );
   }
