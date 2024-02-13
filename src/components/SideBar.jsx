2c4d77126566090755ee57351e37d9d94c69a6a4
olafswan <juliendupdupdup@gmail.com>
2024-02-13 16:36:58 +0100

    react fragments deleted

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,28 +1,15 @@
-import Activity from './Activity';
-
+import Activity from "./Activity";
 
 export default function SideBar() {
-
-    const activitiesList = ["yoga", "swim", "bike", "gym"]
+  const activitiesList = ["yoga", "swim", "bike", "gym"];
 
   return (
-      <div className="side-bar">
-
-        <div className="activities-container">
-         {activitiesList.map(activity => (<>
-         <Activity activity={activity} key={activity}/>
-         
-         <p>{activity}</p>
-         </>
-         )
-         
-         )}
-
-        </div>
-
-
-
-
+    <div className="side-bar">
+      <div className="activities-container">
+        {activitiesList.map((activity, i) => (
+          <Activity activity={activity} key={activity} />
+        ))}
       </div>
+    </div>
   );
 }
